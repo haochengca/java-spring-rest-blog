7f10875603bc602046cffccfede743e7e96b7f30
blueskins@gmail.com <blueskins@gmail.com>
2022-05-04 14:37:43 -0400

    Completed module

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -14,8 +14,11 @@ import java.util.stream.Collectors;
 public class Author {
     public static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
     @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Version
+    private Long version;
     private String firstname;
     private String lastname;
     @JsonIgnore
